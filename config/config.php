fc2a3cfb9f5249e204e541f1bedd603418e7c015
Inggo <inggo.espinosa@gmail.com>
2021-05-26 17:23:31 +0800

    Add package config

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'google_calendar_api_key' => getenv('GCAL_API_KEY');
+];
--- src/TalakdaanServiceProvider.php
+++ src/TalakdaanServiceProvider.php
@@ -8,11 +8,15 @@ class TalakdaanServiceProvider extends ServiceProvider
 {
   public function register()
   {
-    //
+    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'talakdaan');
   }
 
   public function boot()
   {
-    //
+    if ($this->app->runningInConsole()) {
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('talakdaan.php'),
+        ], 'config');
+    }
   }
 }
